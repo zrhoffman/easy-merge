0e7f028b5e42690c22f7aaba9f864eeb152a244e
Zach Hoffman <zach@zrhoffman.net>
2019-10-01 07:39:11 -0500

    Move argument parsing to cli.arguments

--- main.py
+++ main.py
@@ -26,16 +26,10 @@ def is_json(host):
 def main():
     is_json.json_regex = re.compile(r'^application/json')
 
-    parser = argparse.ArgumentParser(description='Merge branches in git easily')  # type: ArgumentParser
-    parser.add_argument('-s', '--source', type=str, help='The source branch to merge from')
-    parser.add_argument('-d', '--dest', type=str, help='The destination branch to merge to')
-    parser.add_argument('-t', '--title', type=str, help='The title of the merge request')
-    parser.add_argument('--description', type=str, help='The description (body) of the merge request')
-    parser.add_argument('-n', '--no-squash', action='store_true', help='If set, do not squash merged commits.')
-    parser.add_argument('-e', '--no-merge', action='store_true', help='If set, do not merge the merge request')
+    arguments.add_parameters()
 
-    # this is up here rather than later because we want to fail early if input does not validate
-    args = parser.parse_args()  # type: Namespace
+    # This is up here rather than later because we want to fail early if input does not validate
+    args = arguments.get_arguments()  # type: Namespace
 
     remote = check_output(['git', 'remote']) \
         .decode('utf-8') \
